71f4f59dad3c9ac82c018fccd4e5c0e24e57984b
Jose Diaz-Gonzalez <jose@seatgeek.com>
2021-02-07 03:06:22 -0500

    refactor: switch to go mod and use bats for testing
    - use go mod and update bindata
    - switch go-bindata to a-urth version which allows us to use the latest and not some weird incompatible tag because of golang modules being spoopy
    - add noop circleci yaml
    - switch from vendored shunit2 to bats
    - switch last access_token querystrings to headers
    - fix release deletion

--- bash/gh-release.bash
+++ bash/gh-release.bash
@@ -8,7 +8,7 @@ release-create() {
 	local release="$(printf "$release_json" "$version" "$name" "$branch")"
 	local release_url="$(printf "$release_endpoint" "$reponame")"
 	echo "Creating release v$version from branch $branch ..."
-	upload_url="$(curl -s -d "$release" "$release_url?access_token=$GITHUB_ACCESS_TOKEN" | upload-url)"
+	upload_url="$(curl -s -H "Authorization: token $GITHUB_ACCESS_TOKEN" -d "$release" "$release_url" | upload-url)"
 	for asset in $(ls -A release); do
 		local name="$(basename $asset)"
 		echo "Uploading $name ..."
@@ -20,10 +20,10 @@ release-create() {
 release-destroy() {
 	declare reponame="$1" version="$2"
 	local release_url="$(printf "$release_endpoint" "$reponame")"
-    
-	[[ "$version" == [0-9]* ]] && version="v$version"
 
-	release_id="$(curl -s "$release_url?access_token=$GITHUB_ACCESS_TOKEN" | release-id-from-tagname "$version")"
+	version="v$version"
+
+	release_id="$(curl -s -H "Authorization: token $GITHUB_ACCESS_TOKEN" "$release_url" | release-id-from-tagname "$version")"
 	echo "Deleting release..."
 	curl -s -H "Authorization: token $GITHUB_ACCESS_TOKEN" -X DELETE "$release_url/$release_id"
 	echo "Deleting tag..."
